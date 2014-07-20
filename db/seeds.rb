66bf9e77c80137d562c3c5cc502a2356cb6e44e2
Drew Robinson <drewbie0307@hotmail.com>
2014-07-20 09:44:47 -0500

    Updated contact form

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,6 @@ Refinery::Pages::Engine.load_seed
 
 # Added by Refinery CMS Cats extension
 Refinery::Cats::Engine.load_seed
+
+# Added by Refinery CMS Inquiries engine
+Refinery::Inquiries::Engine.load_seed
